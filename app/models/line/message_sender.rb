45062d1c0580879aa32ceccc81e6f276ebd3ad2c
vividmuimui <tkoyama@aiming-inc.com>
2016-04-16 06:24:37 +0900

    refactor MessageSender.send_message

--- app/models/line/message_sender.rb
+++ app/models/line/message_sender.rb
@@ -13,25 +13,34 @@ module Line
       end
 
       def send_message(users:, content:)
+        content = build_content(users: users, content: content)
+        logger.debug(content: content)
+        RestClient.proxy = ENV["FIXIE_URL"]
+        RestClient.post(request_url, content.json, request_headers)
+      end
+
+      private
+
+      def build_content(users:, content:)
         content_json = {
           to: Array(users),
           toChannel: 1383378250,  # Fixed  value
           eventType: "138311608800106203", # Fixed  value,
           content: content
-        }.to_json
-        p(content_json: content_json)
+        }
+      end
 
-        RestClient.proxy = ENV["FIXIE_URL"]
-        RestClient.post(
-          'https://trialbot-api.line.me/v1/events',
-          content_json,
-          {
-            'Content-Type' => 'application/json; charset=UTF-8',
-            'X-Line-ChannelID' => ENV["LINE_CHANNEL_ID"],
-            'X-Line-ChannelSecret' => ENV["LINE_CHANNEL_SECRET"],
-            'X-Line-Trusted-User-With-ACL' => ENV["LINE_CHANNEL_MID"],
-          }
-        )
+      def request_url
+        'https://trialbot-api.line.me/v1/events'
+      end
+
+      def request_headers
+        {
+          'Content-Type' => 'application/json; charset=UTF-8',
+          'X-Line-ChannelID' => ENV["LINE_CHANNEL_ID"],
+          'X-Line-ChannelSecret' => ENV["LINE_CHANNEL_SECRET"],
+          'X-Line-Trusted-User-With-ACL' => ENV["LINE_CHANNEL_MID"],
+        }
       end
     end
   end
